f2c8e370f74f8f04fadfe86040b2d6cf3c4e9f2e
Tamago <zekun.jin@foxmail.com>
2024-09-13 17:09:49 +0800

    fix: app header error

--- packages/builder/src/rsbuild.ts
+++ packages/builder/src/rsbuild.ts
@@ -4,18 +4,21 @@ import { config } from './configs/rsbuild.config'
 import { defineBuilder } from './core'
 
 export const rsbuildBuilder = defineBuilder(config, () => {
+  if (!config.html)
+    config.html = {}
+
+
   return {
     async start(ctx) {
       config.server = defu(ctx.options.server ?? {}, config.server)
-      if (!config.html)
-        config.html = {}
-      config.html.title = ctx.options.app?.head?.title ?? ''
+      config.html!.title = ctx.options.app?.head?.title ?? ''
       const rsbuildConfig = defu(ctx.options.rsbuild, config)
       const rsbuild = await createRsbuild({ rsbuildConfig })
       await rsbuild.startDevServer()
     },
 
     async build(ctx) {
+      config.html!.title = ctx.options.app?.head?.title ?? ''
       const rsbuildConfig = defu(ctx.options.rsbuild, config)
       const rsbuild = await createRsbuild({ rsbuildConfig })
       await rsbuild.build()
--- packages/nuxt/src/components/nuxt-welcome/index.vue
+++ packages/nuxt/src/components/nuxt-welcome/index.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen flex flex-col justify-center items-center">
     <div class="mx-auto flex gap-6 font-semibold text-8xl select-none">
       <span>Hello</span>
-      <span class="bg-clip-text text-transparent cursor-default bg-gradient-to-br from-lime-500 to-teal-500">Nuxlite</span>
+      <span class="bg-clip-text text-transparent cursor-default bg-gradient-to-br from-lime-400 to-teal-600">Nuxlite</span>
     </div>
   </div>
 </template>
